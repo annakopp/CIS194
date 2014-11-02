e1b44f3fd75abf20d22a266510893e48d063486d
Anna Kopp <koppanna@gmail.com>
2014-11-01 23:30:00 -0400

    Finishes all the instances

--- hw5/hw5.hs
+++ hw5/hw5.hs
@@ -2,6 +2,11 @@ module Calc where
 
 import Parser
 
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
 data ExprT = Lit Integer
            | Add ExprT ExprT
            | Mul ExprT ExprT
@@ -40,3 +45,21 @@ instance Expr Bool where
     | otherwise = True
   mul = (&&)
   add = (||)
+  
+instance Expr MinMax where
+  lit = MinMax
+  mul (MinMax x) (MinMax y) = lit (min x y)
+  add (MinMax x) (MinMax y) = lit (max x y)
+  
+instance Expr Mod7 where
+  lit x = Mod7 (x `mod` 7)
+  mul (Mod7 x) (Mod7 y) = lit (x+y)
+  add (Mod7 x) (Mod7 y) = lit (x*y)
+  
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger = testExp :: Maybe Integer
+testBool = testExp :: Maybe Bool
+testMM = testExp :: Maybe MinMax
+testSat = testExp :: Maybe Mod7
