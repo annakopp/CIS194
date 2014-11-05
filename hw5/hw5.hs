12e841492699bbf528412d6fffb35f4ae7a5fdaf
Anna Kopp <koppanna@gmail.com>
2014-11-05 00:39:21 -0500

    map to int function is not working :(

--- hw5/hw5.hs
+++ hw5/hw5.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Calc where
 
 import Parser
+import qualified Data.Map as M
 
 newtype MinMax = MinMax Integer deriving (Eq, Show)
 
@@ -63,3 +66,37 @@ testInteger = testExp :: Maybe Integer
 testBool = testExp :: Maybe Bool
 testMM = testExp :: Maybe MinMax
 testSat = testExp :: Maybe Mod7
+
+-- #6
+
+class HasVars a where
+  var :: String -> a
+
+data VarExprT = Lit1 Integer 
+              | Add1 VarExprT VarExprT 
+              | Mul1 VarExprT VarExprT 
+              | Var String
+  deriving (Show, Eq)  
+  
+instance Expr VarExprT where
+  lit = Lit1
+  mul = Mul1
+  add = Add1
+  
+instance HasVars VarExprT where
+  var = Var
+
+instance HasVars (M.Map String Integer -> Maybe Integer) where
+  var str = M.lookup str
+  
+--function f give it a map and itll return an integer  
+  
+-- instance Expr (M.Map String Integer -> Maybe Integer) where
+--   lit = M.lookup . show
+--   mul x y = lit . x
+--   add x y = x
+--
+--test
+
+withVars :: [(String, Integer)] -> (M.Map String Integer -> Maybe Integer) -> Maybe Integer
+withVars vs exp = exp $ M.fromList vs
